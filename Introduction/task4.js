58ded5664a9c248d80b2f66a875ff39eb08fb19e
Joypeterscholar <j.peter@native.semicolon.africa>
2023-06-08 17:37:34 -0400

    initial commit

---
+++ Introduction/task4.js
@@ -0,0 +1,21 @@
+let x = 2;
+let y = 3;
+console.log(2*(x+y));
+
+let a = 20;
+console.log(a%10+a);
+
+
+let b = 5;
+let n = 7;
+console.log(b*10)*n;
+
+
+let d = 25;
+let e = 4; 
+let f = 8;
+let g = 12;
+e = d+10;
+f = 10 -d;
+g = d%10;
+console.log(d/ (e*f*g));
--- Introduction/variables.js
+++ Introduction/variables.js
@@ -1,6 +1,19 @@
 var number = 20;
 let age = 23;
 const SCHOOL = "Semicolon Africa";
-    SCHOOL = "Joy";
     console.log(age);
     console.log(number);
+    var var1 = 2;
+    var var2 = 4;
+    var var3 = 6;
+    var var4;
+
+    var4 = var1
+    var1 = var2;
+    var2 = var3;
+    var3 = free;
+
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+    
